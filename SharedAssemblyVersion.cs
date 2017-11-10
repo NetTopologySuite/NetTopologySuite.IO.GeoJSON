fda47f08535fcdddb8c72409b03ce3c0b25dc5b2
Ben Abelshausen <ben@osmsharp.com>
2017-11-10 10:45:13 +0000

    Initial commit.

---
+++ SharedAssemblyVersion.cs
@@ -0,0 +1,7 @@
+// This is now the main source of versioning information.
+// -> Change the version # here.
+
+using System.Reflection;
+
+[assembly: AssemblyVersion("0.0.1")]
+[assembly: AssemblyFileVersion("0.0.1")]
