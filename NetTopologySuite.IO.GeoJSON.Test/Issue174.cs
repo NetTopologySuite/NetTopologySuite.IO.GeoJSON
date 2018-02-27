64e797b2217b562e41483dbb3120475bcccbe1c0
Ben Abelshausen <ben@osmsharp.com>
2018-02-27 09:51:23 +0000

    Migrated strong naming test.

---
+++ NetTopologySuite.IO.GeoJSON.Test/Issue174.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace NetTopologySuite.IO.GeoJSON.Test
+{
+    class Issue174
+    {
+        [Test, Category("Issue174")]
+        public void ensure_NetTopologySuite_IO_GeoJSON_assembly_is_strongly_named()
+        {
+            AssertStronglyNamedAssembly(typeof(GeoJsonSerializer));
+        }
+
+        private void AssertStronglyNamedAssembly(Type typeFromAssemblyToCheck)
+        {
+            Assert.IsNotNull(typeFromAssemblyToCheck, "Cannot determine assembly from null");
+            Assembly assembly = typeFromAssemblyToCheck.Assembly;
+            StringAssert.DoesNotContain("PublicKeyToken=null", assembly.FullName, "Strongly named assembly should have a PublicKeyToken in fully qualified name");
+        }
+    }
+}
