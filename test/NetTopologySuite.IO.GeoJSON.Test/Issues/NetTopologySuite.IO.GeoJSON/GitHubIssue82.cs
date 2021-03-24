318d83eb4175d6e1a19217b4fd0ac9c376ef5a1e
Felix Obermaier <felix.obermaier@netcologne.de>
2021-03-24 08:23:20 +0100

    Add unit test for issue #82

---
+++ test/NetTopologySuite.IO.GeoJSON.Test/Issues/NetTopologySuite.IO.GeoJSON/GitHubIssue82.cs
@@ -0,0 +1,41 @@
+﻿using NetTopologySuite.Geometries;
+using NetTopologySuite.IO.Converters;
+using Newtonsoft.Json;
+using NUnit.Framework;
+
+namespace NetTopologySuite.IO.GeoJSON.Test.Issues.NetTopologySuite.IO.GeoJSON
+{
+    [GeoJsonIssueNumber(82)]
+    public class GitHubIssue82
+    {
+        [Test]
+        public void TestJsonConverter()
+        {
+            var model = new MyModel
+            {
+                poly = new Polygon(new LinearRing(new[]
+                {
+                    new Coordinate(-100, 45),
+                    new Coordinate(-98, 45),
+                    new Coordinate(-99, 46),
+                    new Coordinate(-100, 45),
+                }))
+            };
+            string geoJson = JsonConvert.SerializeObject(model, Formatting.None,
+                new JsonSerializerSettings
+                {
+                    ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+                    NullValueHandling = NullValueHandling.Ignore,
+                    //Converters = new List<JsonConverter>(new JsonConverter[]{new CoordinateConverter()})
+                });
+
+            Assert.That(geoJson, Is.EqualTo("{\"poly\":{\"type\":\"Polygon\",\"coordinates\":[[[-100.0,45.0],[-98.0,45.0],[-99.0,46.0],[-100.0,45.0]]]}}"));
+        }
+    }
+
+    public class MyModel
+    {
+        [JsonConverter(typeof(GeometryConverter))]
+        public Polygon poly { get; set; }
+    }
+}
